5e6c9c98d72b947ec318c03e957eb2fe0ac40f8e
Ryan Cromwell <ryan@heysparkbox.com>
2021-02-21 21:46:53 -0500

    feat (Rules): Slime always wins

--- js/rules/index.js
+++ js/rules/index.js
@@ -1,9 +1,11 @@
 import { paperBeatsRock } from "./paperBeatsRock.js";
 import { rockBeatsScissors } from "./rockBeatsScissors.js";
 import { scissorsBeatsPaper } from "./scissorsBeatsPaper.js";
+import { slimeAlwaysWins } from "./slimeAlwaysWins.js"
 
 export const rules = [
   paperBeatsRock,
   rockBeatsScissors,
   scissorsBeatsPaper,
+  slimeAlwaysWins,
 ]
